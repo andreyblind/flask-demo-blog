c470d0f330284403aeec68130b01add0c243cfad
Andrii Khanchuk <andreyblind@gmail.com>
2016-05-11 22:00:29 +0300

    added login form

--- app/views.py
+++ app/views.py
@@ -18,3 +18,10 @@ def index():
     return render_template("index.html",
         title = 'Home',
         user = user)
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    form = LoginForm()
+    return render_template('login.html', 
+        title = 'Sign In',
+        form = form)
