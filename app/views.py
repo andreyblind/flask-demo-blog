ca58e6118ed27bcee3eb77167c1a489ca792badc
Andrii Khanchuk <andreyblind@gmail.com>
2016-05-11 21:07:28 +0300

    sceleton app

---
+++ app/views.py
@@ -0,0 +1,15 @@
+from flask import render_template, flash, redirect
+from app import app
+from app.custom_forms import LoginForm
+
+# функция представления index опущена для краткости
+
+# @app.route('/login', methods = ['GET', 'POST'])
+# def login():
+#     form = LoginForm()
+#     return render_template('login.html',
+#         title = 'Sign In',
+#         form = form)
+@app.route('/')
+def start():
+    return "string"
