06da42ae559bec2d0545ea4bbc4a1f94e1c61d72
Andrii Khanchuk <andreyblind@gmail.com>
2016-05-13 16:08:13 +0300

    added flashed messages in base template

--- app/views.py
+++ app/views.py
@@ -2,19 +2,11 @@ from flask import render_template, flash, redirect
 from app import app
 from app.custom_forms import LoginForm
 
-# функция представления index опущена для краткости
-
-# @app.route('/login', methods = ['GET', 'POST'])
-# def login():
-#     form = LoginForm()
-#     return render_template('login.html',
-#         title = 'Sign In',
-#         form = form)
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = { 'nickname': 'Miguel' } # выдуманный пользователь
+    user = { 'nickname': 'Andrii' } # test user
     return render_template("index.html",
         title = 'Home',
         user = user)
